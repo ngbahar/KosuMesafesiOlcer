a555591d509bfaaa183c2166caa4aac9cdea935b
ngbahar <Toshiba@DESKTOP-FB4LU5Q>
2024-05-20 13:44:51 +0300

    Add project files.

---
+++ INTRO-OPSIYONEL/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace INTRO_OPSIYONEL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opsiyonel Soru:Koşuyu bir parkurun etrafında 5 kere koşuyorum. Tüm turları aynı hızda koşmam mümkün olmayacağından her turun sonunda turu koştuğum süreyi kayıt altına almak istiyorum.Koşunun sonunda koştuğun turları en hızlıdan yavaşa doğru sıralayabilir misiniz.
+
+            Basla();
+            Sırala();
+            Cıkıs();
+        }
+
+        static int[] parkur = new int[5];
+        static int sure;
+
+        static void Basla()
+        {
+            Console.WriteLine("Lütfen süreyi dakika cinsinden giriniz.");
+            for (int i = 0; i < parkur.Length; i++)
+            {
+                Console.Write($"{i + 1}. turu giriniz: ");
+                if (int.TryParse(Console.ReadLine(), out sure) && sure > 0 && sure <= 1440) //surenin int olduğunu, sıfırdan küçük olmaması ve dakika cinsinden 24 saati geçmemesi kontrolü  
+                { parkur[i] = sure; }
+                else
+                {
+                    Console.WriteLine("Hatalı giriş yaptınız. Tekrar deneyiniz.");
+                    Basla();
+                }
+            }
+        }
+
+        static void Sırala()
+        {
+            Array.Sort(parkur);
+            foreach (int i in parkur) 
+            { 
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"En hızlı: {parkur[0]} DK \nEn yavaş: {parkur[4]} DK");
+        }
+
+        static void Cıkıs()
+        {
+            Console.WriteLine("Çıkmak isterseniz exit yazın.");
+            string cevap = Console.ReadLine();
+            if (cevap == "exit")
+                Environment.Exit(0);
+            else 
+            {
+                Console.WriteLine("Başlangıç noktasına yönlendiriliyorsunuz....");
+                Basla();
+            }
+
+        
+        }
+    }
+}
---
+++ SINAV-INTRO/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace SINAV_INTRO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Giris();
+            AdımCmAl();
+            DkAdımSayısı();
+            BilgiVer();
+            KosuSuresiSaat();
+            KosuSuresiDakika();
+            Cozum();
+
+        }
+
+        static double adımcm, adımsayısı, saat, dakika, sonuc, saatcevir, toplamsure;
+
+        static void Giris()
+        {
+            Console.WriteLine("*****Koşu Mesafesi Ölçer*****");
+            Console.WriteLine("Bir adımınızın ortalama uzunluğu, dakikada attığınız adım sayısı ve koşu süresi bilgilerini hazırlayınız.");
+        }
+
+        static void AdımCmAl()
+        {
+            Console.Write("Bir adımınızı giriniz(cm):");
+            try  //harf girişi ve sıfırdan küçük girişleri kontrol 
+            {
+                adımcm = double.Parse(Console.ReadLine());
+                if (adımcm <= 0)
+                {
+                    Console.WriteLine("Adım uzunluğu sıfırdan küçük olamaz");
+                    AdımCmAl();
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                AdımCmAl();
+            }
+        }
+
+        static void DkAdımSayısı()
+        {
+            Console.Write("Bir dakikada kaç adım attığınızı giriniz: ");
+            if (double.TryParse(Console.ReadLine(), out adımsayısı) && adımsayısı > 0) //harf girişi ve sıfırdan küçük girişleri kontrol
+            { }
+            else
+            {
+                Console.WriteLine("Hatalı giriş yaptınız. Lütfen tekrar deneyin.");
+                DkAdımSayısı();
+            }
+        }
+
+        static void BilgiVer()
+        { Console.WriteLine("Koşu sürenizi saat ve dakika olmak üzere ayrı girişlerde giriniz."); }
+
+        static void KosuSuresiSaat()
+        {
+
+            Console.Write("Saat: ");
+            if (double.TryParse(Console.ReadLine(), out saat) && saat >= 0 && saat <= 24) //harf girişi ve sıfırdan küçük girişleri ve 24 saati geçmemesi için kontrol
+            { saatcevir = saat * 60; }
+            else
+            {
+                Console.WriteLine("Hatalı giriş yaptınız. Lütfen tekrar deneyin.");
+                KosuSuresiSaat();
+            }
+        }
+
+        static void KosuSuresiDakika()
+        {
+            Console.Write("Dakika: ");
+            if (double.TryParse(Console.ReadLine(), out dakika) && dakika >= 0 && dakika <= 59) //harf girişi ve sıfırdan küçük girişleri ve 59 dakikayı geçmemesi için kontrol
+            { toplamsure = saatcevir + dakika; }
+            else
+            {
+                Console.WriteLine("Hatalı giriş yaptınız. Lütfen tekrar deneyin.");
+                KosuSuresiDakika();
+            }
+        }
+
+        static void Cozum()
+        {
+            sonuc = ((toplamsure) * adımsayısı * (adımcm / 100));
+            Console.WriteLine($"{sonuc} metre koştunuz.");
+            Console.WriteLine("Çıkmak için exit yazınız.");
+            string cıkıs = Console.ReadLine();
+            if (cıkıs == "exit")
+                Environment.Exit(0);
+            else
+                AdımCmAl();
+        }
+
+    }
+}
+
